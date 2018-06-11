b7f558ad55a4f43058f63e1e3ed6ae3df11b4cc4
myroslavarm <romaniuk@ucu.edu.ua>
2018-06-11 12:32:18 +0300

    fixed the script

--- ucubot/Scripts/lesson-signal2.sql
+++ ucubot/Scripts/lesson-signal2.sql
@@ -1,4 +1,4 @@
 ALTER TABLE lesson_signal DROP COLUMN user_id;
 delete from lesson_signal;
 ALTER TABLE lesson_signal ADD COLUMN student_id INT NOT NULL;
-ALTER TABLE lesson_signal ADD CONSTRAINT FOREIGN KEY (student_id) REFERENCES student(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE lesson_signal ADD CONSTRAINT student_const FOREIGN KEY (student_id) REFERENCES student(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
